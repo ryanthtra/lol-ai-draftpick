4df1616600a4d72e8b76528cab45ce590475c0db
Ryan Transfiguracion <ryanthtra@MacBook-Pro.frontierlocal.net>
2018-04-20 08:28:13 -0700

    commented out na1.api... domain for now.  To focus on acs domain APIs.

--- apis.R
+++ apis.R
@@ -1,50 +1,52 @@
 library(httr)
 
-prefix_domain <- "https://na1.api.riotgames.com"
+#prefix_domain <- "https://na1.api.riotgames.com"
 acs_prefix_domain <- "https://acs.leagueoflegends.com/"
 # This api_token will eventually go in a separate file which will be ignored by git.
-api_token <- "RGAPI-afa7ca6b-3ccd-465d-90c4-51616b4fabb8"
+#api_token <- "RGAPI-afa7ca6b-3ccd-465d-90c4-51616b4fabb8"
 
 
-get_summoner_by_name <- function(str_name) {
-  response <- GET(paste(prefix_domain, "/lol/summoner/v3/summoners/by-name/", str_name, sep = ""), add_headers("X-Riot-Token" = api_token))
-  if (response$status_code == 200) {
-    json <- jsonlite::fromJSON(content(response, as = "text"))
-    return (json)
-  }
-}
-
-
-get_matchlist_by_accountid_and_season <- function(num_accountid, num_season) {
-  response <- GET(paste(prefix_domain, "/lol/match/v3/matchlists/by-account/", num_accountid, "?season=", num_season, sep = ""), add_headers("X-Riot-Token" = api_token))
-  if (response$status_code == 200) {
-    json <- jsonlite::fromJSON(content(response, as = "text"))
-    return (json)
-  }
-}
-
-
-get_match_by_matchid <- function(num_matchid) {
-  response <- GET(paste(prefix_domain, "/lol/match/v3/matches/", num_matchid, sep = ""), add_headers("X-Riot-Token" = api_token))
-  if (response$status_code == 200) {
-    json <- jsonlite::fromJSON(content(response, as = "text"))
-    return (json)
-  }
-}
-
-
-get_match_timeline_by_matchid <- function(num_matchid) {
-  response <- GET(paste(prefix_domain, "/lol/match/v3/timelines/by-match/", num_matchid, sep = ""), add_headers("X-Riot-Token" = api_token))
-  if (response$status_code == 200) {
-    json <- jsonlite::fromJSON(content(response, as = "text"))
-    return (json)
-  }
-}
+# get_summoner_by_name <- function(str_name) {
+#   response <- GET(paste(prefix_domain, "/lol/summoner/v3/summoners/by-name/", str_name, sep = ""), add_headers("X-Riot-Token" = api_token))
+#   if (response$status_code == 200) {
+#     json <- jsonlite::fromJSON(content(response, as = "text"))
+#     return (json)
+#   }
+# }
+# 
+# 
+# get_matchlist_by_accountid_and_season <- function(num_accountid, num_season) {
+#   response <- GET(paste(prefix_domain, "/lol/match/v3/matchlists/by-account/", num_accountid, "?season=", num_season, sep = ""), add_headers("X-Riot-Token" = api_token))
+#   if (response$status_code == 200) {
+#     json <- jsonlite::fromJSON(content(response, as = "text"))
+#     return (json)
+#   }
+# }
+# 
+# 
+# get_match_by_matchid <- function(num_matchid) {
+#   response <- GET(paste(prefix_domain, "/lol/match/v3/matches/", num_matchid, sep = ""), add_headers("X-Riot-Token" = api_token))
+#   if (response$status_code == 200) {
+#     json <- jsonlite::fromJSON(content(response, as = "text"))
+#     return (json)
+#   }
+# }
+# 
+# 
+# get_match_timeline_by_matchid <- function(num_matchid) {
+#   response <- GET(paste(prefix_domain, "/lol/match/v3/timelines/by-match/", num_matchid, sep = ""), add_headers("X-Riot-Token" = api_token))
+#   if (response$status_code == 200) {
+#     json <- jsonlite::fromJSON(content(response, as = "text"))
+#     return (json)
+#   }
+# }
 
 
 # API calls using ACS domain
 get_acs_summoner_by_name_and_region <- function(str_name, str_region) {
-  response <- GET(paste(acs_prefix_domain, "/v1/players?name=", str_name, "&region=", str_region, sep = ""))
+  uri <- paste(acs_prefix_domain, "/v1/players?name=", str_name, "&region=", str_region, sep = "")
+  uri
+  response <- GET(uri)
   if (response$status_code == 200) {
     json <- jsonlite::fromJSON(content(response, as = "text"))
     return (json)
@@ -52,25 +54,29 @@ get_acs_summoner_by_name_and_region <- function(str_name, str_region) {
 }
 
 get_acs_player_history_by_platform_and_account_id <- function(chr_platform_id = "EUW1", num_account_id = 23402463) {
-  url <- paste(acs_prefix_domain, "/v1/stats/player_history/", chr_platform_id, "/", num_account_id, sep = "")
-  url
-  response <- GET(url)
+  uri <- paste(acs_prefix_domain, "/v1/stats/player_history/", chr_platform_id, "/", num_account_id, sep = "")
+  uri
+  response <- GET(uri)
   if (response$status_code == 200) {
     json <- jsonlite::fromJSON(content(response, as = "text"))
     return (json)
   }
 }
 
-get_acs_match_by_matchid <- function(chr_platform_id = "NA1", num_match_id = 23402463) {
-  response <- GET(paste(acs_prefix_domain, "/v1/stats/game/", chr_platform_id, "/", num_match_id, sep = ""))
+get_acs_match_by_matchid <- function(chr_platform_id, num_match_id, chr_game_hash = "") {
+  uri <- paste(acs_prefix_domain, "/v1/stats/game/", chr_platform_id, "/", num_match_id, ifelse(chr_game_hash != "", paste("?gameHash=", chr_game_hash, sep = ""), ""), sep = "")
+  uri
+  response <- GET(uri)
   if (response$status_code == 200) {
     json <- jsonlite::fromJSON(content(response, as = "text"))
     return (json)
   }
 }
 
-get_acs_match_timeline_by_matchid <- function(chr_platform_id = "NA1", num_match_id = 23402463) {
-  response <- GET(paste(acs_prefix_domain, "/v1/stats/game/", chr_platform_id, "/", num_match_id, "/timeline", sep = ""))
+get_acs_match_timeline_by_matchid <- function(chr_platform_id, num_match_id, chr_game_hash = "") {
+  uri <- paste(acs_prefix_domain, "/v1/stats/game/", chr_platform_id, "/", num_match_id, "/timeline", ifelse(chr_game_hash != "", paste("?gameHash=", chr_game_hash, sep = ""), ""), sep = "")
+  print(uri)
+  response <- GET(uri)
   if (response$status_code == 200) {
     json <- jsonlite::fromJSON(content(response, as = "text"))
     return (json)
@@ -78,12 +84,19 @@ get_acs_match_timeline_by_matchid <- function(chr_platform_id = "NA1", num_match
 }
 
 
-my_summoner <- get_summoner_by_name("TSM%20Zven")
-my_matchlist <- get_matchlist_by_accountid_and_season(my_summoner$accountId, 11)
-my_match <- get_match_by_matchid(my_matchlist$matches$gameId[1])
-my_timeline <- get_match_timeline_by_matchid(my_matchlist$matches$gameId[1])
+# my_summoner <- get_summoner_by_name("TSM%20Zven")
+# my_matchlist <- get_matchlist_by_accountid_and_season(my_summoner$accountId, 11)
+# my_match <- get_match_by_matchid(my_matchlist$matches$gameId[1])
+# my_timeline <- get_match_timeline_by_matchid(my_matchlist$matches$gameId[1])
+# 
+# acs_summoner <- get_acs_summoner_by_name_and_region("TSM%20Zven", "EUW1")
+# acs_history <- get_acs_player_history_by_platform_and_account_id(chr_platform_id = "EUW1", num_account_id = 23402463)
+# acs_match <- get_acs_match_by_matchid(num_match_id = my_matchlist$matches$gameId[1])
+# acs_timeline <- get_acs_match_timeline_by_matchid(num_match_id = my_matchlist$matches$gameId[1])
 
-acs_summoner <- get_acs_summoner_by_name_and_region("TSM%20Zven", "EUW1")
-acs_history <- get_acs_player_history_by_platform_and_account_id(chr_platform_id = "EUW1", num_account_id = 23402463)
-acs_match <- get_acs_match_by_matchid(num_match_id = my_matchlist$matches$gameId[1])
-acs_timeline <- get_acs_match_timeline_by_matchid(num_match_id = my_matchlist$matches$gameId[1])
+# NA LCS 2018 Spring Split Finals
+nalcs_id = "TRLH1"
+nalcs_finals_gameids <- c(1002530069, 1002530071, 1002530072)
+nalcs_finals_hashes <- c("a774e6c7993c29fa", "f10c8835759ef621", "0e19ce71fe99bf30")
+nalcs_fg1_match <- get_acs_match_by_matchid(nalcs_id, nalcs_finals_gameids[1], chr_game_hash = nalcs_finals_hashes[1])
+nalcs_fg1_timeline <- get_acs_match_timeline_by_matchid(nalcs_id, nalcs_finals_gameids[1], chr_game_hash = nalcs_finals_hashes[1])
