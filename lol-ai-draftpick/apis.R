07bc4cd05bfe29fdf65ceceb8bc8a9aeadf7599f
Ryan Transfiguracion <ryan_transfiguracion@outlook.com>
2018-05-10 03:16:39 -0700

    Went back to using for loops, as they at least produced consistent results compared to sapply/lapply.

--- lol-ai-draftpick/apis.R
+++ lol-ai-draftpick/apis.R
@@ -1,5 +1,6 @@
 library(httr)
 library(dplyr)
+#library(future)
 
 #prefix_domain <- "https://na1.api.riotgames.com"
 
@@ -46,44 +47,43 @@ acs_prefix_domain <- "https://acs.leagueoflegends.com"
 #######################################################
 # API calls using ACS domain
 ######################################
+
+# API call helper from response
+process_uri <- function(str_uri) {
+
+  print(str_uri)
+  #future_response <- future({
+  #return (GET(str_uri))
+  #}) %plan% multiprocess
+  #response <- value(future_response)
+  response <- GET(str_uri)
+  print(response$status_code)
+  while (response$status_code == 429) {
+    Sys.sleep(2)
+    response <- GET(str_uri)
+  }
+  json <- jsonlite::fromJSON(content(response, as = "text"))
+  return(json)
+}
+
 get_acs_summoner_by_name_and_region <- function(str_name, str_region) {
   uri <- paste(acs_prefix_domain, "/v1/players?name=", str_name, "&region=", str_region, sep = "")
-  print(uri)
-  response <- GET(uri)
-  if (response$status_code == 200) {
-    json <- jsonlite::fromJSON(content(response, as = "text"))
-    return(json)
-  }
+  return (process_uri(uri))
 }
 
 get_acs_player_history_by_platform_and_account_id <- function(chr_platform_id = "EUW1", num_account_id = 23402463) {
   uri <- paste(acs_prefix_domain, "/v1/stats/player_history/", chr_platform_id, "/", num_account_id, sep = "")
-  print(uri)
-  response <- GET(uri)
-  if (response$status_code == 200) {
-    json <- jsonlite::fromJSON(content(response, as = "text"))
-    return(json)
-  }
+  return(process_uri(uri))
 }
 
 get_acs_match_by_matchid <- function(chr_platform_id, num_match_id, chr_game_hash = "") {
   uri <- paste(acs_prefix_domain, "/v1/stats/game/", chr_platform_id, "/", num_match_id, ifelse(chr_game_hash != "", paste("?gameHash=", chr_game_hash, sep = ""), ""), sep = "")
-  print(uri)
-  response <- GET(uri)
-  if (response$status_code == 200) {
-    json <- jsonlite::fromJSON(content(response, as = "text"))
-    return(json)
-  }
+  return(process_uri(uri))
 }
 
 get_acs_match_timeline_by_matchid <- function(chr_platform_id, num_match_id, chr_game_hash = "") {
   uri <- paste(acs_prefix_domain, "/v1/stats/game/", chr_platform_id, "/", num_match_id, "/timeline", ifelse(chr_game_hash != "", paste("?gameHash=", chr_game_hash, sep = ""), ""), sep = "")
-  print(uri)
-  response <- GET(uri)
-  if (response$status_code == 200) {
-    json <- jsonlite::fromJSON(content(response, as = "text"))
-    return(json)
-  }
+  return(process_uri(uri))
 }
 ######################################
 # End -- API methods
@@ -94,16 +94,28 @@ get_acs_match_timeline_by_matchid <- function(chr_platform_id, num_match_id, chr
 ######################################
 
 get_league_match_data_list <- function(league_matchid_df) {
-  #matchlist <- apply(league_matchid_df[, c('Region.ID', 'Game.ID', 'Hash.ID', 'Blue.Team', 'Red.Team')], 1, function(row) {
+  # OLD FOR LOOP IMPLEMENTATION
+  matchlist <- list()
+  for (i in 1:nrow(league_matchid_df)) {
+    matchlist[[i]] <- get_acs_match_by_matchid(league_matchid_df$Region.ID[[i]], league_matchid_df$Game.ID[[i]], chr_game_hash = league_matchid_df$Hash.ID[[i]])
+  }
+
+  #matchlist <- apply(league_matchid_df[c(1:10), c('Region.ID', 'Game.ID', 'Hash.ID', 'Blue.Team', 'Red.Team')], 1, function(row) {
   #current_match <- get_acs_match_by_matchid(row[1], row[2], chr_game_hash = row[3])
-  
+  #Sys.sleep(1.0)
   #return (current_match)
   #})
-  matchlist <- sapply(1:nrow(nalcs_matchid_df), function(i) {
-    current_match <- get_acs_match_by_matchid(nalcs_matchid_df$Region.ID[[i]], nalcs_matchid_df$Game.ID[[i]], chr_game_hash = nalcs_matchid_df$Hash.ID[[i]])
-    
-    return(current_match)
-  })
+
+  ##matchlist <- sapply(1:10, function(i) {
+  #matchlist <- sapply(1:nrow(league_matchid_df), function(i) {
+    ##print(paste("API Call #", i, sep=""))
+  #current_match <- get_acs_match_by_matchid(league_matchid_df$Region.ID[[i]], league_matchid_df$Game.ID[[i]], chr_game_hash = league_matchid_dfHash.ID[[i]])
+
+    ##if ((i %% 14) == 0) {
+    ##Sys.sleep(0.5)
+    ##}
+    #return(current_match)
+  #})
   
   return(matchlist)
 }
@@ -116,30 +128,34 @@ get_league_match_data_list <- function(league_matchid_df) {
 # NA LCS 2018 Spring Split -- Regular Season and Playoffs
 nalcs_matchid_df <- read.csv("NALCS_Spring2018.csv")
 # EU LCS 2018 Spring Split -- Regular Season and Playoffs
-eulcs_matchid_df <- read.csv("EULCS_Spring2018.csv")
+#eulcs_matchid_df <- read.csv("EULCS_Spring2018.csv")
 
-
-# OLD FOR LOOP IMPLEMENTATION
-#for (i in 1:length(nalcs_f_matchids$gameids)) {
-#nalcs_f_matches[[paste("game", i, sep="")]] <- get_acs_match_by_matchid(nalcs_id, nalcs_f_matchids$gameids[i], chr_game_hash = nalcs_f_matchids$hashes[i])
-#nalcs_f_timelines[i] <- get_acs_match_timeline_by_matchid(nalcs_id, nalcs_f_matchids$gameids[i], chr_game_hash = nalcs_f_matchids$hashes[i])
-#}
-
-#nalcs_matches <- get_league_match_data_list(nalcs_matchid_df)
+nalcs_matches <- get_league_match_data_list(nalcs_matchid_df)
 
 nalcs_single_match <- get_acs_match_by_matchid(nalcs_matchid_df$Region.ID[[1]], nalcs_matchid_df$Game.ID[[1]], chr_game_hash = nalcs_matchid_df$Hash.ID[[1]])
 
 # Get the "teams" data frame, which contains who won/lost, first blood, first baron, etc.
 # Will need to wrangle so that team names are in each row, "Team 100/200" is changed to Blue/Red,
 # and each entry in the list is concatenated into a large list, in order to do data visualization.
+#nalcs_matches_teams <- list()
+#nalcs_matches_teams <- sapply(1:ncol(nalcs_matches), function(i) {
 #nalcs_matches_teams <- sapply(1:length(nalcs_matches), function(i) {
   ##Add team name column
   ##nalcs_matches[[i]]$teams["teamNames"] <- c(nalcs_matchid_df[i, c("Blue.Team", "Red.Team")])
+  #nalcs_matches[[i]]$teams["teamNames"] <- unname(unlist(c(nalcs_matchid_df[i, c("Blue.Team", "Red.Team")])))
+  ##nalcs_matches[, i]$teams["teamNames"] <- unname(unlist(c(nalcs_matchid_df[i, c("Blue.Team", "Red.Team")])))
   #return(nalcs_matches[[i]]$teams)
+  ##return(nalcs_matches[, i]$teams)
 #})
-#nalcs_matches_teams_accum <- data.frame()
-#for (i in 1:length(nalcs_matches_teams)) {
-  #nalcs_matches_teams_accum <- nalcs_matches_teams_accum %>% bind_rows(nalcs_matches_teams[i])
+nalcs_matches_teams_accum <- data.frame(NULL)
+for (i in 1:length(nalcs_matches)) {
+  nalcs_matches[[i]]$teams["teamNames"] <- unname(unlist(c(nalcs_matchid_df[i, c("Blue.Team", "Red.Team")])))
+  #nalcs_matches_teams[[i]] <- nalcs_matches[[i]]$teams
+  nalcs_matches_teams_accum <- nalcs_matches_teams_accum %>% bind_rows(nalcs_matches[[i]]$teams)
+}
+#nalcs_matches_teams_accum <- nalcs_matches_teams[[1]]
+#for (i in 2:length(nalcs_matches_teams)) {
+  #nalcs_matches_teams_accum <- nalcs_matches_teams_accum %>% bind_rows(nalcs_matches_teams[[i]])
 #}
 #nalcs_single_match$teams["teamNames"] <- c(nalcs_matchid_df[1, c("Blue.Team", "Red.Team")])
 #nalcs_single_match$teams["teamNames"] <- as.character(c(nalcs_matchid_df[1, c("Blue.Team", "Red.Team")]))
